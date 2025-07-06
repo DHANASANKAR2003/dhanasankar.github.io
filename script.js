07374b8fc11d557e686b39dcb374f61db5bc4acc
DHANASANKAR K <146157469+DHANASANKAR2003@users.noreply.github.com>
2025-07-06 20:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,6 +79,9 @@ document.addEventListener("DOMContentLoaded", () => {
   closeBtn.onclick = () => {
     modal.style.display = "none";
   };
+  modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.style.display = "none";
+});
 
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") modal.style.display = "none";
